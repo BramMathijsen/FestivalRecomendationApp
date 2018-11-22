a1e88226eac20da2d595fefaf840d225e0dd1dab
H1ddez <37654532+H1ddez@users.noreply.github.com>
2018-11-22 14:43:58 +0100

    testcases toegevoegd aan sp's

--- Constraints/Artiest/sp_delete_artiest.sql
+++ Constraints/Artiest/sp_delete_artiest.sql
@@ -15,3 +15,15 @@ BEGIN
 	THROW;
 	END CATCH
 END
+
+-- ===================================================================
+-- Testcase
+-- ===================================================================
+
+-- een artiest verwijderen verwijderen
+
+EXEC sp_delete_artiest 'afrojack'
+
+SELECT * FROM Artiest WHERE artiest_naam = 'afrojack'
+SELECT * FROM Bezetting_van_artiest WHERE artiest_naam = 'afrojack'
+SELECT * FROM bezetting WHERE bezetting_id IN (SELECT bezetting_id FROM Bezetting_van_artiest WHERE artiest_naam = 'afrojack')
