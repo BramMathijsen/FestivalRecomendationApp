9da15ad363748c1da93ba0a38a17b8f4fa5eed9e
H1ddez <37654532+H1ddez@users.noreply.github.com>
2018-11-22 14:13:32 +0100

    Update pr_alter_gebruiker_gegevens.sql

--- Constraints/Gebruiker/pr_alter_gebruiker_gegevens.sql
+++ Constraints/Gebruiker/pr_alter_gebruiker_gegevens.sql
@@ -19,31 +19,15 @@ BEGIN
 	IF(@gebruikers_voornaam IS NULL) SELECT @emailadres = (SELECT emailadres FROM gebruiker WHERE gebruikersnaam = @gebruikersnaam)
 
 	UPDATE gebruiker
-	SET @gebruikers_voornaam = gebruikers_voornaam AND
-		@gebruikers_tussenvoegsel = ebruikers_tussenvoegsel AND
-		@gebruikers_achternaam = gebruikers_achternaam AND
-		@geboortedatum = geboortedatum AND
-		@woonplaats = woonplaats AND
-		@wachtwoord = wachtwoor AND
+	SET @gebruikers_voornaam = gebruikers_voornaam,
+		@gebruikers_tussenvoegsel = gebruikers_tussenvoegsel,
+		@gebruikers_achternaam = gebruikers_achternaam,
+		@geboortedatum = geboortedatum,
+		@woonplaats = woonplaats,
+		@wachtwoord = wachtwoord,
 		@emailadres = emailadres
 	WHERE gebruikersnaam = @gebruikersnaam
 
-	END TRY
-	BEGIN CATCH
-	THROW;
-	END CATCH
-END
-
-GO
-CREATE PROC pr_alter_gebruikersnaam 
-	@gebruikersnaam_oud VARCHAR(50), 
-	@gebruikersnaam_nieuw VARCHAR(50)
-AS
-BEGIN
-	BEGIN TRY
-		UPDATE gebruikersnaam
-		SET gebruikersnaam = @gebruikersnaam_nieuw
-		WHERE gebruikersnaam = @gebruikersnaam_oud
 	END TRY
 	BEGIN CATCH
 	THROW;
